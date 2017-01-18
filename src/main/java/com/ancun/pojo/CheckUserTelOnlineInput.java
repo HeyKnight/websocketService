077efe75611655ce1bcde856766cf725d31f3b65
jianzhiyuan <1543069294@qq.com>
2017-01-18 19:45:15 +0800

    文件名大小写

---
+++ src/main/java/com/ancun/pojo/CheckUserTelOnlineInput.java
@@ -0,0 +1,20 @@
+package com.ancun.pojo;
+
+/**
+ * checkUserTelOnlineInput类
+ *
+ * @author jarvan [NO.0051]
+ * @since 2017年01月18日 18:59
+ */
+public class CheckUserTelOnlineInput {
+
+    private String userTel;
+
+    public String getUserTel() {
+        return userTel;
+    }
+
+    public void setUserTel(String userTel) {
+        this.userTel = userTel;
+    }
+}
